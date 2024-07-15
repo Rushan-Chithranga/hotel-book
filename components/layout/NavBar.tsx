0f5c031318199e9ed8f3ffcde1a63bca284abb43
rushan <rushan@thesanmark.com>
2024-07-15 13:09:40 +0530

    #1 feat:Clerk Auth setup

---
+++ components/layout/NavBar.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+
+const NavBar = () => {
+  return (
+    <div>
+      <UserButton afterSwitchSessionUrl="/" />
+    </div>
+  );
+};
+export default NavBar;
